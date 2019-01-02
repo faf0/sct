61bda46f3186adc03e3f443d052210b6c4d262d8
zvezdochiot <mykaralw@yandex.ru>
2019-01-02 19:09:00 +0300

    1.3 "rename for X11"
    Rename utilites for X11.

--- sct.c
+++ sct.c
@@ -1,8 +1,8 @@
 /*
- * sct - set color temperature
+ * xsct - X11 set color temperature
  *
  * Compile the code using the following command:
- * cc -std=c99 -O2 -I /usr/X11R6/include -o sct sct.c -L /usr/X11R6/lib -lm -lX11 -lXrandr
+ * cc -std=c99 -O2 -I /usr/X11R6/include sct.c -o xsct -L /usr/X11R6/lib -lX11 -lXrandr
  *
  * Original code published by Ted Unangst:
  * http://www.tedunangst.com/flak/post/sct-set-color-temperature
@@ -18,94 +18,108 @@
  * Public domain, do as you wish.
  */
 
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 #include <X11/Xatom.h>
 #include <X11/Xlib.h>
 #include <X11/Xproto.h>
 #include <X11/extensions/Xrandr.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void
+usage()
+{
+    printf("Usage: xsct [temperature]\n"
+        "Temperatures must be in a range from 1000-10000\n"
+        "If no arguments are passed xsct resets the display to the default temperature (6500K)\n"
+        "If -h or --help is passed xsct will display this usage information\n");
+    exit(0);
+}
+
 /* cribbed from redshift, but truncated with 500K steps */
 static const struct { float r; float g; float b; } whitepoints[] = {
-	{ 1.00000000,  0.18172716,  0.00000000, }, /* 1000K */
-	{ 1.00000000,  0.42322816,  0.00000000, },
-	{ 1.00000000,  0.54360078,  0.08679949, },
-	{ 1.00000000,  0.64373109,  0.28819679, },
-	{ 1.00000000,  0.71976951,  0.42860152, },
-	{ 1.00000000,  0.77987699,  0.54642268, },
-	{ 1.00000000,  0.82854786,  0.64816570, },
-	{ 1.00000000,  0.86860704,  0.73688797, },
-	{ 1.00000000,  0.90198230,  0.81465502, },
-	{ 1.00000000,  0.93853986,  0.88130458, },
-	{ 1.00000000,  0.97107439,  0.94305985, },
-	{ 1.00000000,  1.00000000,  1.00000000, }, /* 6500K */
-	{ 0.95160805,  0.96983355,  1.00000000, },
-	{ 0.91194747,  0.94470005,  1.00000000, },
-	{ 0.87906581,  0.92357340,  1.00000000, },
-	{ 0.85139976,  0.90559011,  1.00000000, },
-	{ 0.82782969,  0.89011714,  1.00000000, },
-	{ 0.80753191,  0.87667891,  1.00000000, },
-	{ 0.78988728,  0.86491137,  1.00000000, }, /* 10000K */
-	{ 0.77442176,  0.85453121,  1.00000000, },
+    { 1.00000000,  0.18172716,  0.00000000, }, /* 1000K */
+    { 1.00000000,  0.42322816,  0.00000000, },
+    { 1.00000000,  0.54360078,  0.08679949, },
+    { 1.00000000,  0.64373109,  0.28819679, },
+    { 1.00000000,  0.71976951,  0.42860152, },
+    { 1.00000000,  0.77987699,  0.54642268, },
+    { 1.00000000,  0.82854786,  0.64816570, },
+    { 1.00000000,  0.86860704,  0.73688797, },
+    { 1.00000000,  0.90198230,  0.81465502, },
+    { 1.00000000,  0.93853986,  0.88130458, },
+    { 1.00000000,  0.97107439,  0.94305985, },
+    { 1.00000000,  1.00000000,  1.00000000, }, /* 6500K */
+    { 0.95160805,  0.96983355,  1.00000000, },
+    { 0.91194747,  0.94470005,  1.00000000, },
+    { 0.87906581,  0.92357340,  1.00000000, },
+    { 0.85139976,  0.90559011,  1.00000000, },
+    { 0.82782969,  0.89011714,  1.00000000, },
+    { 0.80753191,  0.87667891,  1.00000000, },
+    { 0.78988728,  0.86491137,  1.00000000, }, /* 10000K */
+    { 0.77442176,  0.85453121,  1.00000000, },
 };
 
 static void
 sct_for_screen(Display *dpy, int screen, int temp)
 {
-	Window root = RootWindow(dpy, screen);
-	XRRScreenResources *res = XRRGetScreenResourcesCurrent(dpy, root);
+    Window root = RootWindow(dpy, screen);
+    XRRScreenResources *res = XRRGetScreenResourcesCurrent(dpy, root);
 
-	temp -= 1000;
-	double ratio = temp % 500 / 500.0;
+    temp -= 1000;
+    double ratio = temp % 500 / 500.0;
 #define AVG(c) whitepoints[temp / 500].c * (1 - ratio) + whitepoints[temp / 500 + 1].c * ratio
-	double gammar = AVG(r);
-	double gammag = AVG(g);
-	double gammab = AVG(b);
+    double gammar = AVG(r);
+    double gammag = AVG(g);
+    double gammab = AVG(b);
 
-	for (int c = 0; c < res->ncrtc; c++) {
-		int crtcxid = res->crtcs[c];
-		int size = XRRGetCrtcGammaSize(dpy, crtcxid);
+    for (int c = 0; c < res->ncrtc; c++) {
+        int crtcxid = res->crtcs[c];
+        int size = XRRGetCrtcGammaSize(dpy, crtcxid);
 
-		XRRCrtcGamma *crtc_gamma = XRRAllocGamma(size);
+        XRRCrtcGamma *crtc_gamma = XRRAllocGamma(size);
 
-		for (int i = 0; i < size; i++) {
-			double g = 65535.0 * i / size;
-			crtc_gamma->red[i] = g * gammar;
-			crtc_gamma->green[i] = g * gammag;
-			crtc_gamma->blue[i] = g * gammab;
-		}
+        for (int i = 0; i < size; i++) {
+            double g = 65535.0 * i / size;
+            crtc_gamma->red[i] = g * gammar;
+            crtc_gamma->green[i] = g * gammag;
+            crtc_gamma->blue[i] = g * gammab;
+        }
 
-		XRRSetCrtcGamma(dpy, crtcxid, crtc_gamma);
-		XFree(crtc_gamma);
-	}
+        XRRSetCrtcGamma(dpy, crtcxid, crtc_gamma);
+        XFree(crtc_gamma);
+    }
 
-	XFree(res);
+    XFree(res);
 }
 
 int
 main(int argc, char **argv)
 {
-	Display *dpy = XOpenDisplay(NULL);
-	if (!dpy) {
-		perror("XOpenDisplay(NULL) failed");
-		fprintf(stderr, "Make sure DISPLAY is set correctly.\n");
-		return EXIT_FAILURE;
-	}
-	int screens = XScreenCount(dpy);
+    Display *dpy = XOpenDisplay(NULL);
+    if (!dpy) {
+        perror("XOpenDisplay(NULL) failed");
+        fprintf(stderr, "Make sure DISPLAY is set correctly.\n");
+        return EXIT_FAILURE;
+    }
+    int screens = XScreenCount(dpy);
 
-	int temp = 6500;
-	if (argc > 1)
-		temp = atoi(argv[1]);
-	if (temp < 1000 || temp > 10000)
-		temp = 6500;
+    int temp = 6500;
+    if (argc > 1)
+    {
+        if (!strcmp(argv[1],"-h") || !strcmp(argv[1],"--help"))
+            usage();
+        temp = atoi(argv[1]);
+    }
+    if (temp < 1000 || temp > 10000)
+        temp = 6500;
 
-	for (int screen = 0; screen < screens; screen++)
-		sct_for_screen(dpy, screen, temp);
+    for (int screen = 0; screen < screens; screen++)
+        sct_for_screen(dpy, screen, temp);
 
-	XCloseDisplay(dpy);
+    XCloseDisplay(dpy);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
