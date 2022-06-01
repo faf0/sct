9ba6ce9e9e082432455990d77caf1044e9ef7d7d
Fabian Foerg <3429782+faf0@users.noreply.github.com>
2022-05-31 21:14:21 -0400

    Trim xsct.c lead comment

--- xsct.c
+++ xsct.c
@@ -1,22 +1,7 @@
 /*
  * xsct - X11 set color temperature
  *
- * Original code published by Ted Unangst:
- * http://www.tedunangst.com/flak/post/sct-set-color-temperature
- *
- * Modified by Fabian Foerg in order to:
- * - compile on Ubuntu 14.04
- * - iterate over all screens of the default display and change the color
- *   temperature
- * - fix memleaks
- * - clean up code
- * - return EXIT_SUCCESS
- *
  * Public domain, do as you wish.
- *
- * Compile the code using the following command:
- * gcc -Wall -Wextra -Werror -pedantic -std=c99 -O2 -I /usr/X11R6/include xsct.c -o xsct -L /usr/X11R6/lib -lX11 -lXrandr -lm -s
- *
  */
 
 #include <X11/Xatom.h>
